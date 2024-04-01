842854e65b10d916bb3f221c6b56ba09ee31aaa0
Александр <@STORM>
2024-04-01 16:50:52 +0300

    rows all

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -4,11 +4,16 @@ using namespace std;
 
 void Allocate(int** arr, const int rows, const int cols);
 void Randl_arr(int** arr, const int rows, const int cols);
-void Randl_rows(int** arr, int row, const int cols);
+void Randl_row(int** arr, const int row, const int cols);
 void Print(int** arr, const int rows, const int cols);
 void Clear(int** arr, const int rows);
 void push_row_back(int**& arr,  int & rows, const int cols);
 void push_row_front(int**& arr,  int & rows, const int cols);
+void insert_row(int**& arr, int& rows, const int cols, const int index );
+void pop_row_back(int**& arr,  int & rows, const int cols);
+void pop_row_front(int**& arr,  int & rows, const int cols);
+void erase_row(int**& arr, int& rows, const int cols, const int index );
+
 
 void main()
 {
@@ -26,6 +31,24 @@ void main()
 	Print(arr, rows,cols);
 	push_row_front(arr, rows, cols);
 	Print(arr, rows,cols);
+	int index;
+	do
+	{
+		cout << "¬ведите индекс дл€ вставки строки в массив от 0 до " << rows - 1<<" "; cin >> index;
+	} while (index>=rows);
+	insert_row(arr, rows, cols, index);
+	Print(arr, rows,cols);
+	pop_row_back(arr, rows, cols);
+	Print(arr, rows,cols);
+	pop_row_front(arr, rows, cols);
+	Print(arr, rows,cols);
+	do
+	{
+		cout << "¬ведите индекс дл€ удаление строки из массив от 0 до " << rows - 1<<" "; cin >> index;
+	} while (index>=rows);
+	erase_row(arr, rows,cols,index);
+	Print(arr, rows,cols);
+
 	Clear(arr, rows);
 
 }
@@ -51,7 +74,7 @@ void Randl_arr(int** arr, const int rows, const int cols)
 {
 	for (int i = 0; i < rows; i++) for (int j = 0; j < cols; j++) arr[i][j] = rand() % 100;
 }
-void Randl_rows(int** arr, int row, const int cols)
+void Randl_row(int** arr, const int row, const int cols)
 {
 	for (int i = 0; i < cols; i++) arr[row][i] = rand() % 100;
 }
@@ -61,7 +84,7 @@ void push_row_back(int**&arr, int& rows, const int cols)
 	Allocate(bufer, rows+1, cols);
 	for (int i = 0; i < rows; i++) for (int j = 0; j < cols; j++) bufer[i][j] = arr[i][j];
 	Clear(arr, rows);
-	Randl_rows(bufer, rows,cols);
+	Randl_row(bufer, rows,cols);
 	rows++;
 	arr = bufer;
 }
@@ -71,8 +94,44 @@ void push_row_front(int**& arr, int& rows, const int cols)
 	Allocate(bufer, rows + 1, cols);
 	for (int i = 0; i < rows; i++) for (int j = 0; j < cols; j++) bufer[i+1][j] = arr[i][j];
 	Clear(arr, rows);
-	Randl_rows(bufer, rows-rows, cols);
+	Randl_row(bufer, rows-rows, cols);
 	rows++;
 	arr = bufer;
-	//Print(arr, rows,cols);
+}
+void insert_row(int**& arr, int& rows, const int cols, const int index)
+{
+	int** bufer = new int* [rows+1];
+	Allocate(bufer, rows+1, cols);
+	for (int i = 0; i < rows; i++) for (int j = 0; j < cols; j++) i < index ? bufer[i][j] = arr[i][j] : bufer[i + 1][j] = arr[i][j];
+	Clear(arr, rows);
+	Randl_row(bufer, index, cols);
+	rows++;
+	arr = bufer;
+}
+void pop_row_back(int**& arr, int& rows, const int cols)
+{
+	int** bufer = new int* [rows - 1];
+	Allocate(bufer, rows - 1, cols);
+	for (int i = 0; i < rows-1; i++) for (int j = 0; j < cols; j++) bufer[i][j] = arr[i][j];
+	Clear(arr, rows);
+	--rows;
+	arr = bufer;
+}
+void pop_row_front(int**& arr, int& rows, const int cols)
+{
+	int** bufer = new int* [rows - 1];
+	Allocate(bufer, rows - 1, cols);
+	for (int i = 0; i < rows-1; i++) for (int j = 0; j < cols; j++) bufer[i][j] = arr[i+1][j];
+	Clear(arr, rows);
+	--rows;
+	arr = bufer;
+}
+void erase_row(int**& arr, int& rows, const int cols, const int index)
+{
+	int** bufer = new int* [rows - 1];
+	Allocate(bufer, rows - 1, cols);
+	for (int i = 0; i < rows-1; i++) for (int j = 0; j < cols; j++) i < index ? bufer[i][j] = arr[i][j] : bufer[i][j] = arr[i+1][j];
+	Clear(arr, rows);
+	--rows;
+	arr = bufer;
 }
